1e3fb0ff3e1c24be0c2950ddfaab7ccbb6efbd2a
thdk <t.dekiere@gmail.com>
2021-04-16 12:28:59 +0200

    test: fix typescript errors in AvatarInput tests

--- app/frontend/src/components/AvatarInput.test.tsx
+++ app/frontend/src/components/AvatarInput.test.tsx
@@ -64,7 +64,7 @@ describe("AvatarInput component", () => {
   });
 
   it("uploads upon confirmation and submits key", async () => {
-    userEvent.upload(screen.getByLabelText(SELECT_AN_IMAGE), MOCK_FILE);
+    userEvent.upload(screen.getByLabelText(SELECT_AN_IMAGE) as HTMLInputElement, MOCK_FILE);
 
     expect(await screen.findByLabelText(CONFIRM_UPLOAD)).toBeVisible();
 
@@ -85,7 +85,7 @@ describe("AvatarInput component", () => {
   });
 
   it("cancels when cancel button pressed and doesn't submit key", async () => {
-    userEvent.upload(screen.getByLabelText(SELECT_AN_IMAGE), MOCK_FILE);
+    userEvent.upload(screen.getByLabelText(SELECT_AN_IMAGE) as HTMLInputElement, MOCK_FILE);
 
     expect(await screen.findByLabelText(CANCEL_UPLOAD)).toBeVisible();
 
@@ -117,7 +117,7 @@ describe("AvatarInput component", () => {
     });
 
     //first upload and confirm
-    userEvent.upload(screen.getByLabelText(SELECT_AN_IMAGE), OTHER_MOCK_FILE);
+    userEvent.upload(screen.getByLabelText(SELECT_AN_IMAGE) as HTMLInputElement, OTHER_MOCK_FILE);
     expect(await screen.findByLabelText(CONFIRM_UPLOAD)).toBeVisible();
     userEvent.click(screen.getByLabelText(CONFIRM_UPLOAD));
 
@@ -129,7 +129,7 @@ describe("AvatarInput component", () => {
     ).toMatch(/thumb0.jpg/);
 
     //2nd upload and cancel
-    userEvent.upload(screen.getByLabelText(SELECT_AN_IMAGE), MOCK_FILE);
+    userEvent.upload(screen.getByLabelText(SELECT_AN_IMAGE) as HTMLInputElement, MOCK_FILE);
     expect(await screen.findByLabelText(CANCEL_UPLOAD)).toBeVisible();
     userEvent.click(screen.getByLabelText(CANCEL_UPLOAD));
     expect(
@@ -145,7 +145,7 @@ describe("AvatarInput component", () => {
   });
 
   it("doesn't submit without confirming/cancelling", async () => {
-    userEvent.upload(screen.getByLabelText(SELECT_AN_IMAGE), MOCK_FILE);
+    userEvent.upload(screen.getByLabelText(SELECT_AN_IMAGE) as HTMLInputElement, MOCK_FILE);
 
     expect(await screen.findByLabelText(CONFIRM_UPLOAD)).toBeVisible();
 
@@ -160,7 +160,7 @@ describe("AvatarInput component", () => {
       FileReader.prototype.dispatchEvent(new Event("error"));
     });
     userEvent.upload(
-      screen.getByLabelText(SELECT_AN_IMAGE),
+      screen.getByLabelText(SELECT_AN_IMAGE) as HTMLInputElement,
       new File([new Blob(undefined)], "")
     );
     expect(await screen.findByText(INVALID_FILE)).toBeVisible();
@@ -170,7 +170,7 @@ describe("AvatarInput component", () => {
     uploadFileMock.mockRejectedValueOnce(new Error("Whoops"));
     jest.spyOn(console, "error").mockReturnValueOnce(undefined);
     userEvent.upload(
-      screen.getByLabelText(SELECT_AN_IMAGE),
+      screen.getByLabelText(SELECT_AN_IMAGE) as HTMLInputElement,
       new File([new Blob(undefined)], "")
     );
     expect(await screen.findByLabelText(CONFIRM_UPLOAD)).toBeVisible();
@@ -182,11 +182,11 @@ describe("AvatarInput component", () => {
   //This doesn't work https://github.com/testing-library/user-event/issues/632
   //We reset by setting input.value = "" but this doesn't do anything for @testing-library
   it.skip("previews the image after cancelling and selecting the same image", async () => {
-    userEvent.upload(screen.getByLabelText(SELECT_AN_IMAGE), MOCK_FILE);
+    userEvent.upload(screen.getByLabelText(SELECT_AN_IMAGE) as HTMLInputElement, MOCK_FILE);
     expect(await screen.findByLabelText(CANCEL_UPLOAD)).toBeVisible();
     userEvent.click(screen.getByLabelText(CANCEL_UPLOAD));
 
-    userEvent.upload(screen.getByLabelText(SELECT_AN_IMAGE), MOCK_FILE);
+    userEvent.upload(screen.getByLabelText(SELECT_AN_IMAGE) as HTMLInputElement, MOCK_FILE);
 
     expect(await screen.findByLabelText(CANCEL_UPLOAD)).toBeVisible();
     expect(
